6af246959432449cabc9b5a48a8ce7c62445b6fc
Ignacio Buioli <ibuioli@gmail.com>
2020-06-21 22:44:50 -0300

    feat: network scanner

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
+declare var jquery: any;
+declare var $: any;
+declare var navigator: any;
+declare var window: any;
+declare var cordova: any;
+
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
@@ -20,16 +26,51 @@ export class ScannerComponent implements OnInit, OnChanges {
   public showStock = false;
   ////////////////////////////
   safe = 'normal';
+  timer = 0;
+  connect: any;
 
   constructor() { }
 
   // Lifehooks funs
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.scanNetwork();
+    this.connect = setInterval(() => {
+      this.scanNetwork();
+    }, 3000);
+  }
 
   public ngOnChanges(): void {
     if (this.logged) {
     }
+  }
+
+  public scanNetwork(): void {
+    const start_time = new Date().getTime();
+    $.xmlrpc({
+      url: this.server + '/common',
+      methodName: 'version',
+      crossDomain: true,
+      params: [],
+      success: (response: any, status: any, jqXHR: any) => {
+        const request_time = new Date().getTime() - start_time;
+        this.timer = request_time;
+        if (request_time <= 150) {
+          this.safe = 'good';
+        } else if (request_time > 150 && request_time <= 450) {
+          this.safe = 'normal';
+        } else if (request_time > 450 && request_time <= 800) {
+          this.safe = 'slow';
+        } else if (request_time > 800 && request_time <= 1500) {
+          this.safe = 'bad';
+        } else {
+          this.safe = 'critical';
+        }
+      },
+      error: (jqXHR: any, status: any, error: any) => {
+        console.log('Error : ' + error );
+      }
+    });
   }
 
   // END - Lifehooks funs
@@ -38,10 +79,12 @@ export class ScannerComponent implements OnInit, OnChanges {
 
   public picking(): void {
     this.showPicking = true;
+    clearInterval(this.connect);
   }
 
   public stock(): void {
     this.showStock = true;
+    clearInterval(this.connect);
   }
 
   // END - Internal use funs
@@ -49,6 +92,10 @@ export class ScannerComponent implements OnInit, OnChanges {
   public goOut(): void {
     this.showPicking = false;
     this.showStock = false;
+    this.scanNetwork();
+    this.connect = setInterval(() => {
+      this.scanNetwork();
+    }, 3000);
   }
 
   /* LogOut emiter */
